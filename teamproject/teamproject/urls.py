f5b315c2303d6ae9164d9aec71e7fa225ebd468b
Den <den@den-laptop.(none)>
2012-06-07 14:42:39 +0400

    modified urls and added css

--- teamproject/teamproject/urls.py
+++ teamproject/teamproject/urls.py
@@ -1,21 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from teamproject.views import main, employee, projects, article, employee_one, search
+#from teamproject.views import main, employee, projects, article, employee_one, search
 from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	(r'^admin/', include(admin.site.urls)),
-	#TODO: make mail page root
-	url(r'^main/$',main, name='teamproject_main'),
-	url(r'^employee/$',employee, name='teamproject_employee'),
-	url(r'^employee/(\d{1,2})',employee_one, name='teamproject_employee_view'),
-	url(r'^projects/$',projects, name='teamproject_projects'),
-	url(r'^article/$',article, name='teamproject_article'),
+	#(SOLVED)TODO: make mail page root
+)
+urlpatterns += patterns('teamproject.views',
+	url(r'^main/$','main', name='teamproject_main'),
+	url(r'^employee/$','employee', name='teamproject_employee'),
+	url(r'^employee/(?P<offset>\d{1,2})','employee_one', name='teamproject_employee_view'),
+	url(r'^projects/$','projects', name='teamproject_projects'),
+	url(r'^article/$','article', name='teamproject_article'),
 	#url(r'^search-form/$', search_form, nameen/employee/='teamproject_search-form'),
-    url(r'^search/$', search, name='teamproject_search'),
-    # url(r'^$', 'teamproject.views.home', name='home'),
+    url(r'^search/$', 'search', name='teamproject_search'),
+    url(r'^$', 'search', name='home'),
     # url(r'^teamproject/', include('teamproject.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
